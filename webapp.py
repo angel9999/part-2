2c313cd2ffcd095e9c4e69c523baa10891bfcebf
Angel <angelC9999@gmail.com>
2018-11-09 10:28:30 -0800

    i

--- webapp.py
+++ webapp.py
@@ -6,22 +6,33 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
-    return render_template('2.html', my_variable="test")
-
+    if "States" in request.args :
+        state = request.args["States"]
+        return render_template('2.html',state = get_state_options(), random = get_random(state))
+    else:
+        return render_template('2.html',state = get_state_options())
 
 def get_state_options():
     with open('county_demographics.json') as demographics_data:
         counties = json.load(demographics_data)
     list=[]
-    for each county in counties:
-        if !(county["state"] in list):
-            list+=county["state"]
+    for county in counties:
+        if not(county["State"] in list):
+            list.append(county["State"])
     a=""
     option=""
-    for each state in list:
+    for state in list:
         option = option + Markup("<option value=\"" + state + "\">" + state + "</option>")
     return option
-        
+    
+def get_random(state):
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data)
+    sum =0
+    for county in counties :
+        if county["State"] == state:
+            sum += county["Housing"]["Housing Units"]
+    return ( state + " has " + str(sum) + " houses")
 
 if __name__=="__main__":
     app.run(debug=True, port=54321)
